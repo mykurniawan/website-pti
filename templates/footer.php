f5e8ae43d5b285ff387e39a5899c1a0cd1afdf54
M. Yoga Kurniawan <kurniawan.y09a@gmail.com>
2025-10-01 09:54:36 +0700

    push

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,8 @@
     <div class="container py-5">
         <div class="row g-5">
             <div class="col-lg-6 col-md-6">
-                <h4 class="text-light mb-4">Address</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Jl. Mertojoyo Blok L, Merjosari, Kec. Lowokwaru, Kota Malang, Jawa Timur 65144</p>
+                <h4 class="text-light mb-4"><i class="fa fa-map-marked-alt me-2"></i> Address</h4>
+                <p class="mb-2">Jl. Mertojoyo Blok L, Merjosari, Kec. Lowokwaru, Kota Malang, Jawa Timur 65144</p>
                 <!-- <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p> -->
                 <p class="mb-2"><i class="fa fa-envelope me-3"></i>pti@unigamalang.ac.id</p>
                 <a style="text-decoration: none; color: white;" href="https://www.unigamalang.ac.id/" target="_blank"><i class="fa fa-globe me-3"></i>unigamalang.ac.id</a> <br>
@@ -16,14 +16,14 @@
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
-                <h4 class="text-light mb-4"><i class="fa-solid fa-alarm-clock"></i>Opening Hours</h4>
+                <h4 class="text-light mb-4"><i class="fa fa-clock me-2"></i> Opening Hours</h4>
                 <h6 class="text-light">Senin - Kamis:</h6>
                 <p class="mb-4">08.00 AM - 02.00 PM</p>
                 <h6 class="text-light">Jum'at:</h6>
                 <p class="mb-0">08.00 AM - 11.00 AM</p>
             </div>
             <div class="col-lg-3 col-md-6">
-                <h4 class="text-light mb-4"> Visit </h4>
+                <h4 class="text-light mb-4"><i class="fa fa-globe me-2"></i> Visit </h4>
                 <a href="https://kemdiktisaintek.go.id/" target="_blank">kemdiktisaintek.go.id</a><br>
                 <a href="https://pddikti.kemdiktisaintek.go.id/" target="_blank">pddikti.kemdiktisaintek.go.id</a> <br>
                 <a href="https://lldikti7.kemdikbud.go.id/" target="_blank">LLDikti7.kemdikbud.go.id</a> <br>
@@ -38,10 +38,6 @@
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                     &copy; <span id="year"></span> <a class="border-bottom" href="#">pti.unigamalang.ac.id</a>, All Right Reserved.
                 </div>
-                <!-- <div class="col-md-6 text-center text-md-end">
-                              /*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/
-                              Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a>
-                          </div> -->
             </div>
         </div>
     </div>
